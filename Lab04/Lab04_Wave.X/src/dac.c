d78b40fa52342b0b92319a34a2f4649afbd92bc8
Samuele Ribaudo <samuele.ribaudo@tum.de>
2025-11-27 14:05:29 +0100

    try

--- Lab04/Lab04_Wave.X/src/dac.c
+++ Lab04/Lab04_Wave.X/src/dac.c
@@ -22,19 +22,75 @@
 #define DAC_SCK_AD2CFG AD2PCFGLbits.PCFG11
 #define DAC_LDAC_AD2CFG AD2PCFGLbits.PCFG13
 
+
 void dac_initialize()
 {
     // set AN10, AN11 AN13 to digital mode
+    DAC_SDI_AD1CFG = 1;
+    DAC_SDI_AD2CFG = 1;
+    DAC_SCK_AD1CFG = 1;
+    DAC_SCK_AD2CFG = 1;
+    DAC_LDAC_AD1CFG = 1;
+    DAC_LDAC_AD2CFG = 1;
+
     // this means AN10 will become RB10, AN11->RB11, AN13->RB13
     // see datasheet 11.3
     
     // set RD8, RB10, RB11, RB13 as output pins
+    DAC_CS_TRIS = 0;
+    DAC_SDI_TRIS = 0;
+    DAC_SCK_TRIS = 0;
+    DAC_LDAC_TRIS = 0;
     
-    // set default state: CS=on, SCK=off, SDI=off, LDAC=on
+    // set default state: CS=??, SCK=??, SDI=??, LDAC=??
+    DAC_CS_PORT = 1; // should be set low for the duration of a write command
+    DAC_SDI_PORT = 0; // don't care
+    DAC_SCK_PORT = 0; // clock set on rising edge (startitng position is 0)
+    DAC_LDAC_PORT = 1; // to avoid data transfer to DAC
 
 }
 
+
+void dac_send(uint16_t dac_value)
+{
+    DAC_CS_PORT = 0; // begin serial transmission;
+    int i;
+    for(i = 15; i >= 0; i--){
+        // serially upload register value
+        DAC_SDI_PORT = (dac_value >> i) & 1; // take the i bit value
+        Nop();
+        DAC_SCK_PORT = 1; // rise the clock
+        Nop();
+        DAC_SCK_PORT = 0;
+        Nop();
+    }
+    DAC_CS_PORT = 1; // end transmission
+    DAC_LDAC_PORT = 0; // transfer the register value from LDAC to DAC
+    Nop();
+    DAC_LDAC_PORT = 1;
+}
+
+
 void dac_convert_milli_volt(uint16_t milliVolt)
 {
+    float vout = milliVolt / 1000.0f;
+
+    // page 22 MCP4822-datasheet.pdf
+    uint16_t dac_value = 0; // we are usign channel A (bit 15 == 0);
+    float vref = 2.048; // reference voltage in volts page 4 MCP4822-datasheet.pdf
     
+    if(vout > 2*vref) vout = 2*vref; // max value we can represent
+    if(vout < 0) vout = 0; // vout should be positive
+    
+    if(vout <= vref){
+        // gain = 1
+        dac_value = (uint16_t) ((1 << 12) * vout/vref) | (1 << 13); // basically the formula at page 22 of MCP4822-datasheet.pdf + bit 13 value
+    }else{
+        // gain = 2
+        dac_value = (uint16_t) ((1 << 12)* vout/(vref*2)) | (0 << 13);
+    }
+    
+    dac_value |= (1 << 12);
+
+    dac_send(dac_value);
 }
--- Lab04/Lab04_Wave.X/src/lab04.c
+++ Lab04/Lab04_Wave.X/src/lab04.c
@@ -12,28 +12,6 @@
 #include "led.h"
 #include "dac.h"
 
-// tristate register
-#define DAC_CS_TRIS TRISDbits.TRISD8
-#define DAC_SDI_TRIS TRISBbits.TRISB10
-#define DAC_SCK_TRIS TRISBbits.TRISB11
-#define DAC_LDAC_TRIS TRISBbits.TRISB13
-
-// port register
-#define DAC_CS_PORT PORTDbits.RD8
-#define DAC_SDI_PORT PORTBbits.RB10
-#define DAC_SCK_PORT PORTBbits.RB11
-#define DAC_LDAC_PORT PORTBbits.RB13
-
-// analog to digital converter 1 port configuration register
-#define DAC_SDI_AD1CFG AD1PCFGLbits.PCFG10
-#define DAC_SCK_AD1CFG AD1PCFGLbits.PCFG11
-#define DAC_LDAC_AD1CFG AD1PCFGLbits.PCFG13
-
-// analog to digital converter 2 port configuration register
-#define DAC_SDI_AD2CFG AD2PCFGLbits.PCFG10
-#define DAC_SCK_AD2CFG AD2PCFGLbits.PCFG11
-#define DAC_LDAC_AD2CFG AD2PCFGLbits.PCFG13
-
 // signal parameter
 #define sine_freq 20.0f
 #define sample_rate 1000.0f
@@ -71,36 +49,6 @@ void timer_initialize()
     T3CONbits.TON = 1;
 }
 
-void dac_send(uint16_t command)
-{
-    DAC_CS_PORT = 0;
-    
-    int i;
-    for (i = 15; i >= 0; i--) {
-        DAC_SDI_PORT = (command >> i) & 1;
-        DAC_SCK_PORT = 1;
-        Nop(); Nop();
-        
-        DAC_SCK_PORT = 0;
-        Nop();
-    }
-    
-    DAC_CS_PORT = 1;
-    
-    DAC_LDAC_PORT = 0;
-    Nop(); Nop();
-    DAC_LDAC_PORT = 1;
-}
-
-
-void dac_set_voltage(float voltage)
-{
-    uint16_t code = (uint16_t)((voltage / 4.096f) * 4095.0f);
-    uint16_t command = 0x3000 | (code & 0x0FFF);
-    
-    dac_send(command);
-}
-
 
 void __attribute__((__interrupt__, __auto_psv__)) _T3Interrupt(void) {
     // Calculate Current Time t
@@ -115,7 +63,7 @@ void __attribute__((__interrupt__, __auto_psv__)) _T3Interrupt(void) {
     float v_out = (v_amp * sin(omega * t)) + v_offset;
 
     // Send to DAC
-    dac_set_voltage(v_out);
+    dac_convert_milli_volt((uint16_t)(v_out * 1000.0f));
 
     // Toggle LED1
     TOGGLELED(LED1_PORT);
