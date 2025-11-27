0e8ce365228e4879bc9e22dfa98e344978a33f28
Venkata Kotte <go56xur@mytum.de>
2025-11-27 13:54:24 +0100

    Edit dac.c

--- Lab04/Lab04_Wave.X/src/dac.c
+++ Lab04/Lab04_Wave.X/src/dac.c
@@ -27,10 +27,24 @@ void dac_initialize()
     // set AN10, AN11 AN13 to digital mode
     // this means AN10 will become RB10, AN11->RB11, AN13->RB13
     // see datasheet 11.3
+    DAC_SDI_AD1CFG = 1;
+    DAC_SDI_AD2CFG = 1;
+    DAC_SCK_AD1CFG = 1;
+    DAC_SCK_AD2CFG = 1;
+    DAC_LDAC_AD1CFG = 1;
+    DAC_LDAC_AD2CFG = 1;
     
     // set RD8, RB10, RB11, RB13 as output pins
+    DAC_CS_TRIS = 0;
+    DAC_SDI_TRIS = 0;
+    DAC_SCK_TRIS = 0;
+    DAC_LDAC_TRIS = 0;
     
     // set default state: CS=on, SCK=off, SDI=off, LDAC=on
+    DAC_CS_PORT = 1; // should be set low for the duration of a write command
+    DAC_SDI_PORT = 0; // don't care
+    DAC_SCK_PORT = 0; // clock set on rising edge (startitng position is 0)
+    DAC_LDAC_PORT = 1; // to avoid data transfer to DAC
 
 }
 
@@ -38,3 +52,4 @@ void dac_convert_milli_volt(uint16_t milliVolt)
 {
     
 }
+
